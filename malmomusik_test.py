2ef260a74e917d849afd30ba8dd1c30c85b9975f
SimonJagerSwe <123063643+SimonJagerSwe@users.noreply.github.com>
2023-03-24 00:12:50 +0100

    Add files via upload

--- malmomusik_test.py
+++ malmomusik_test.py
@@ -6,6 +6,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 from helper_tests_malmomusik import simple_assert, boolean_assert
 
 MAIN_SITE = "https://www.malmomusikaffar.com/"
+ELEC_GUIT = "https://www.malmomusikaffar.com/stranginstrument/gitarr/elgitarrer"
+T484 = "https://www.malmomusikaffar.com/eastman-t484-classic-t484"
 
 @pytest.fixture
 def load_driver():
@@ -15,3 +17,51 @@ def load_driver():
     yield driver
 
     driver.quit()
+    
+
+def assert_open_time(load_driver):
+
+    driver = load_driver
+
+    driver.get(MAIN_SITE)
+
+    heading = driver.find_element(By.XPATH, "/html/body/div[3]/footer/section/ul/li[1]/header/span")
+
+    simple_assert(heading.text, "M-F: 10-18 L: 10-14")
+
+
+def find_T484(load_driver):
+
+    driver = load_driver
+
+    driver.get(ELEC_GUIT)
+
+    T484_link = driver.find_element(By.LINK_TEXT, "T484 Classic")
+
+    T484_link.click()
+
+    heading = driver.find_element(By.XPATH, "/html/body/div[3]/main/div/div/section/div/div[3]/div[2]/h1/span")
+
+    simple_assert(heading.text, "T484 Classic")
+
+
+def add_to_cart(load_driver):
+
+    driver = load_driver
+
+    driver.get(T484)
+
+    buy_button = driver.find_element(By.XPATH, "/html/body/div[3]/main/div/div/section/div/div[3]/div[4]/div[2]/form/div/div/div[2]/button/span")
+
+    buy_button.click()
+
+
+def find_on_fb(load_driver):
+
+    driver = load_driver
+
+    driver.get(MAIN_SITE)
+
+    fb_link = driver.find_element(By.XPATH, "/html/body/div[3]/footer/div[1]/div[2]/ul/li[1]/a/svg/use")
+
+    fb_link.click()
